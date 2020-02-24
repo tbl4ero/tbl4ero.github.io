621bd258192f7fe24b3ce76202ff4f13bd04524a
Dev <cartman21000@gmail.com>
2020-02-24 14:06:58 +0900

    routes fixed

--- precache-manifest.096071a39d9301cc4f746c6b0dd04f24.js
+++ precache-manifest.096071a39d9301cc4f746c6b0dd04f24.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "0908d64110e82b1476b95d2e10f5c869",
-    "url": "/portfolio/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "6ce296e9b1d488530d58",
-    "url": "/portfolio/static/css/main.6feff7fe.chunk.css"
+    "url": "/static/css/main.6feff7fe.chunk.css"
   },
   {
     "revision": "af3dc2465bf8998d6b0e",
-    "url": "/portfolio/static/js/2.6579bb92.chunk.js"
+    "url": "/static/js/2.6579bb92.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/portfolio/static/js/2.6579bb92.chunk.js.LICENSE"
+    "url": "/static/js/2.6579bb92.chunk.js.LICENSE"
   },
   {
     "revision": "6ce296e9b1d488530d58",
-    "url": "/portfolio/static/js/main.bb30ae7c.chunk.js"
+    "url": "/static/js/main.bb30ae7c.chunk.js"
   },
   {
     "revision": "0d98471f6c3b69ca4251",
-    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+    "url": "/static/js/runtime-main.44087e9b.js"
   }
 ]);
