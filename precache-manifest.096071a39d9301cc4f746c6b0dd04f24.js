87dd8f79c5f84ab53f50d58b61464696ac93106b
Dev <cartman21000@gmail.com>
2020-02-24 14:03:52 +0900

    new build

---
+++ precache-manifest.096071a39d9301cc4f746c6b0dd04f24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0908d64110e82b1476b95d2e10f5c869",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ce296e9b1d488530d58",
+    "url": "/portfolio/static/css/main.6feff7fe.chunk.css"
+  },
+  {
+    "revision": "af3dc2465bf8998d6b0e",
+    "url": "/portfolio/static/js/2.6579bb92.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.6579bb92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ce296e9b1d488530d58",
+    "url": "/portfolio/static/js/main.bb30ae7c.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
